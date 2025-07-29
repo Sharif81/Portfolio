9ccc96f917853c9e6b4303469456429d0a27db81
sharif81 <sharifahamedalif@gmail.com>
2025-07-29 14:01:15 +0600

    Changes font color

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -17,7 +17,7 @@ const Home = () => {
                     className="max-w-sm rounded-lg shadow-2xl" alt="This is hero Images" width={400} />
                 <div>
                     <TypingEffect text={texts} speed={100} />
-                    <p className="py-4 text-gray-800">
+                    <p className="py-4">
                         I am an experienced ASP.NET Developer skilled in building dynamic web applications. My expertise includes technologies such as SQL Server, Oracle, JavaScript, jQuery, Bootstrap, Tailwind, and GitHub. I prioritize clean code, performance optimization, and responsive design, delivering high-quality solutions that effectively meet user and business needs. Additionally, I have experience with RDLC reports and am continuously learning new technologies to enhance my skill set.
                     </p>
                     <button className="btn btn-outline btn-primary">Hire Me</button>
